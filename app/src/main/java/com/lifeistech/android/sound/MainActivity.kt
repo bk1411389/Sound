07a2dc5b8d88f8e7e4563e820b05078a5d53b0d3
Watarai <is0288xs@ed.ritsumei.ac.jp>
2019-11-01 05:55:23 +0900

    サウンドアプリ完成

---
+++ app/src/main/java/com/lifeistech/android/sound/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.lifeistech.android.sound
+
+import android.media.MediaPlayer
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.taiko)
+
+        imageView.setOnClickListener{
+            mediaPlayer.start()
+        }
+    }
+}
